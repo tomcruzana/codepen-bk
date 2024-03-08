01d2a74d6062a04260e6fb319ca3951675d475ac
tomcruzana <tomcruzana@ymail.com>
2024-03-08 08:48:13 -0500

    added game of skate dice

---
+++ game-of-skate-dice/README.md
@@ -0,0 +1,4 @@
+# Game of Skate Dice
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/thomascruzana/pen/xxMBRJY](https://codepen.io/thomascruzana/pen/xxMBRJY).
+
---
+++ game-of-skate-dice/dist/script.js
@@ -0,0 +1,88 @@
+const playDiceRollSound = (url) => {
+    var a = new Audio(url);
+    a.play();
+}
+
+const stances = [
+  "natural stance", 
+  "switch",
+  "fakie",
+  "nollie",
+  "any stance"
+]
+
+const sides = [
+  "frontside", 
+  "backside"
+]
+
+const spins = [
+  "-",
+  "180", 
+  "360",
+  "540",
+  "-",
+  "-"
+]
+
+const flips = [
+  "-",
+  "pop shuvit", 
+  "heelflip",
+  "kickflip",
+  "hardflip",
+  "inward heelflip",
+  "varial kickflip",
+  "varial heelflip",
+  "bigspin",
+  "bigflip",
+  "impossible",
+  "360 flip",
+  "lazer flip"
+]
+
+const rollDice = () => {
+  const stanceVal = Math.floor(Math.random() * 5)
+  const sideVal = Math.floor(Math.random() * 2)
+  const spinVal = Math.floor(Math.random() * 6)
+  const flipVal = Math.floor(Math.random() * 13)
+  
+  return {
+    stanceVal,
+    sideVal,
+    spinVal,
+    flipVal
+  }
+}
+
+const stanceDice = document.querySelector(".stances-dice")
+const sideDice = document.querySelector(".sides-dice")
+const spinDice = document.querySelector(".spins-dice")
+const flipDice = document.querySelector(".flips-dice")
+    
+const rollBtn = document.querySelector("#roll")
+
+rollBtn.addEventListener("click", (e)=>{
+  playDiceRollSound('https://bit.ly/dice-sound')
+  e.preventDefault;
+  stanceDice.classList.remove("rotate")
+  sideDice.classList.remove("rotate")
+  spinDice.classList.remove("rotate")
+  flipDice.classList.remove("rotate")
+  
+  void stanceDice.offsetWidth;
+  void sideDice.offsetWidth;
+  void spinDice.offsetWidth;
+  void flipDice.offsetWidth;
+  
+  stanceDice.classList.add("rotate")
+  sideDice.classList.add("rotate")
+  spinDice.classList.add("rotate")
+  flipDice.classList.add("rotate")
+  
+  stanceDice.innerHTML = stances[rollDice().stanceVal]
+  sideDice.innerHTML = sides[rollDice().sideVal]
+  spinDice.innerHTML = spins[rollDice().spinVal]
+  flipDice.innerHTML = flips[rollDice().flipVal]
+  
+})

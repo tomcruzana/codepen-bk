7e09221fb94e9fa8004aa01b40a9d662e8b8a9cb
tomcruzana <tomcruzana@ymail.com>
2024-03-09 23:42:12 -0500

    added bible verse gen

---
+++ bible-verse-generator/README.md
@@ -0,0 +1,4 @@
+# Bible Verse Generator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/thomascruzana/pen/qBgvXQq](https://codepen.io/thomascruzana/pen/qBgvXQq).
+
---
+++ bible-verse-generator/dist/script.js
@@ -0,0 +1,16 @@
+//bible verse
+const v = document.querySelector(".verse");
+const c = document.querySelector(".chapter");
+const b = document.querySelector("button");
+const audio = new Audio('https://www.fesliyanstudios.com/play-mp3/387');
+
+b.addEventListener("click", ()=>{
+  audio.play(); //sound 
+  fetch("https://beta.ourmanna.com/api/v1/get/?format=json&order=random")
+  .then(r => r.json())
+  .then(d => {
+        v.textContent = `"${d.verse.details.text}"`;
+        c.textContent = d.verse.details.reference;
+  })
+  .catch(err => alert(`error fetching data ${err}`))
+});
